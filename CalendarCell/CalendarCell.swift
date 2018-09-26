6479c790973910ad7342c8959b3ededcba934c73
Furkan Torun <ftorun@gmail.com>
2018-09-26 10:53:05 +0300

    update1

--- CalendarCell/CalendarCell.swift
+++ CalendarCell/CalendarCell.swift
@@ -14,9 +14,11 @@ import Eureka
 
 
 public class CalendarCell: Cell<Set<Date>>, CellType,FSCalendarDelegate,FSCalendarDataSource {
-
+    
     @IBOutlet weak var calendar: FSCalendar!
     @IBOutlet weak var lblSelectedDates: UILabel!
+    
+    open var lastSelectedDate: Date!
     
     fileprivate lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -27,6 +29,7 @@ public class CalendarCell: Cell<Set<Date>>, CellType,FSCalendarDelegate,FSCalend
     open override func setup() {
         height = { 400 }
         row.title = nil
+        calendar.isHidden = row.isDisabled
         super.setup()
         selectionStyle = .none
         calendar.allowsMultipleSelection = true
@@ -34,17 +37,35 @@ public class CalendarCell: Cell<Set<Date>>, CellType,FSCalendarDelegate,FSCalend
         calendar.delegate = self
     }
     
+    open override func update() {
+        
+        row.title = nil
+        super.update()
+        
+        if( row.value != nil)
+        {
+            for date in Array(row.value!) {
+                calendar.select(date)
+            }
+        }
+        
+        
+        
+    }
+    
     public func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        
         labelUpdate()
     }
     
     public func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        
         labelUpdate()
     }
     
     public func labelUpdate()
     {
-        
+        row.value = Set(calendar.selectedDates.map{ $0 })
         var st:String = ""
         
         for date in calendar.selectedDates {
